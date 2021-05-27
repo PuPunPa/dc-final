9ccfa40868ff343e21e8204a364e0d1a84ecf23d
Jose Pablo Lopez Portillo <josepablolppl@gmail.com>
2021-05-27 04:47:38 +0000

    Update Dependencies
    Signed-off-by: Jose Pablo Lopez Portillo <josepablolppl@gmail.com>

--- main.go
+++ main.go
@@ -6,9 +6,9 @@ import (
 	"math/rand"
 	"time"
 
-	"github.com/CodersSquad/dc-final/api"
-	"github.com/CodersSquad/dc-final/controller"
-	"github.com/CodersSquad/dc-final/scheduler"
+	"github.com/PuPunPa/dc-final/api"
+	"github.com/PuPunPa/dc-final/controller"
+	"github.com/PuPunPa/dc-final/scheduler"
 )
 
 func main() {
