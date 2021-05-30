3efa65cd6503e95da8525792103b579c5830a03d
Jose Pablo Lopez Portillo <josepablolppl@gmail.com>
2021-05-30 23:52:18 +0000

    API <-> Controller communication works
    Signed-off-by: Jose Pablo Lopez Portillo <josepablolppl@gmail.com>

--- api/api.go
+++ api/api.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"encoding/json"
+	"fmt"
 	_ "image/jpeg"
 	_ "image/png"
 	"log"
@@ -14,7 +16,28 @@ import (
 	//go get github.com/gorilla/mux
 	//to fix it
 	"github.com/gorilla/mux"
+	"go.nanomsg.org/mangos"
+	"go.nanomsg.org/mangos/protocol/pair"
 )
+
+type Workload struct {
+	ID             int    `json:"ID"`
+	Filter         string `json:"Filter"`
+	Name           string `json:"Name"`
+	Status         string `json:"Status"`
+	RunningJobs    int    `json:"RunningJobs"`
+	FilteredImages []int  `json:"FilteredImages"`
+}
+
+type WorkloadsResponse struct {
+	Workload []Workload
+	Response string
+}
+
+type Response struct {
+	Workload Workload
+	Response string
+}
 
 // Logged: Structure to store logged in users info, key is Token, value is User
 var online = make(map[string]string)
@@ -27,21 +50,10 @@ type Image struct {
 	size              int
 }
 
-// Global variable user. All functions are able to access to it
-func Start() {
-	router := mux.NewRouter()
-	users["username"] = "password"
-	//All routes for API
-	//Need functions
-	router.HandleFunc("/login", login)                         //POST
-	router.HandleFunc("/logout", logout)                       //DELETE
-	router.HandleFunc("/status", status)                       //GET
-	router.HandleFunc("/workloads", workloads)                 //POST
-	router.HandleFunc("/workloads/{workload_id}", getWorkload) //GET
-	router.HandleFunc("/images", images)                       //POST
-	router.HandleFunc("/images/{image_id}", getImage)          //GET
-	http.ListenAndServe(":8080", router)
-}
+var socket mangos.Socket
+
+var controllerAddress = "tcp://localhost:40899"
+var apiAddress = "tcp://localhost:40900"
 
 func login(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-type", "application/json")
@@ -88,25 +100,7 @@ func checkPass(usr string, pwd string) bool {
 
 func logout(w http.ResponseWriter, r *http.Request) {
 	token := r.Header.Get("Authorization")
-	if len(token) < 7 {
-		w.WriteHeader(http.StatusUnauthorized)
-		msg := `
-{
-	"Please enter a token" ` + token + `smiles
-}
-`
-		w.Write([]byte(msg))
-		return
-	}
-	token = token[7:]
-	if !loggedIn(token) {
-		w.WriteHeader(http.StatusUnauthorized)
-		msg := `
-{
-	"Please enter a valid token"
-}
-`
-		w.Write([]byte(msg))
+	if !checkToken(w, r) {
 		return
 	}
 	w.WriteHeader(http.StatusOK)
@@ -128,32 +122,13 @@ func loggedIn(token string) bool {
 }
 
 func status(w http.ResponseWriter, r *http.Request) {
-	token := r.Header.Get("Authorization")
-	if len(token) < 7 {
-		w.WriteHeader(http.StatusUnauthorized)
-		msg := `
-{
-	"Please enter a token" ` + token + `
-}
-`
-		w.Write([]byte(msg))
-		return
-	}
-	token = token[7:]
-	if !loggedIn(token) {
-		w.WriteHeader(http.StatusUnauthorized)
-		msg := `
-{
-	"Please enter a valid token"
-}
-`
-		w.Write([]byte(msg))
+	if !checkToken(w, r) {
 		return
 	}
 	w.WriteHeader(http.StatusOK)
 	name, err := os.Hostname()
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Fatal("API Error: " + err.Error())
 	}
 	msg := `
 {
@@ -165,20 +140,103 @@ func status(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(msg))
 	return
 }
+
+func images(w http.ResponseWriter, r *http.Request) {
+	if !checkToken(w, r) {
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	return
+}
+
 func workloads(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-type", "application/json")
+	if !checkToken(w, r) {
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	name := r.Header.Get("WorkloadName")
+	filter := r.Header.Get("Filter")
+	sendMessage(socket, "1_"+name+"_"+filter)
+	rMsg := receiveMessage(socket)
+	workloadResp := new(WorkloadsResponse)
+	err := json.Unmarshal([]byte(rMsg), &workloadResp)
+	if err != nil {
+		log.Fatal("API Error: " + err.Error())
+	}
+	workloads := workloadResp.Workload
+	respResp := workloadResp.Response
+	if respResp != "" {
+		w.Write([]byte(respResp))
+		return
+	}
+	for _, wl := range workloads {
+		msg, err := json.MarshalIndent(wl, " ", "    ")
+		if err != nil {
+			log.Fatal("API error: " + err.Error())
+		}
+		w.Write([]byte(msg))
+		w.Write([]byte("\n"))
+	}
 	return
 }
 
 func getWorkload(w http.ResponseWriter, r *http.Request) {
-	return
-}
-
-func images(w http.ResponseWriter, r *http.Request) {
+	if !checkToken(w, r) {
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	id := vars["workload_id"]
+	sendMessage(socket, "2_"+id)
+	rMsg := receiveMessage(socket)
+	workloadResp := new(Workload)
+	err := json.Unmarshal([]byte(rMsg), &workloadResp)
+	if err != nil {
+		log.Fatal("API Error: " + err.Error())
+	}
+	msg, err := json.MarshalIndent(workloadResp, " ", "    ")
+	if err != nil {
+		log.Fatal("API error: " + err.Error())
+	}
+	w.Write([]byte(msg))
+	w.Write([]byte("\n"))
 	return
 }
 
 func getImage(w http.ResponseWriter, r *http.Request) {
+	if !checkToken(w, r) {
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 	return
+}
+
+func checkToken(w http.ResponseWriter, r *http.Request) bool {
+	token := r.Header.Get("Authorization")
+	if len(token) < 7 {
+		w.WriteHeader(http.StatusUnauthorized)
+		msg := `
+{
+	"Please enter a token" ` + token + `
+}
+`
+		w.Write([]byte(msg))
+		return false
+	}
+	token = token[7:]
+	if !loggedIn(token) {
+		w.WriteHeader(http.StatusUnauthorized)
+		msg := `
+{
+	"Please enter a valid token"
+}
+`
+		w.Write([]byte(msg))
+		return false
+	}
+	return true
 }
 
 const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
@@ -192,4 +250,45 @@ func GenerateRandomString(n int) string {
 		sb.WriteByte(letterBytes[idx])
 	}
 	return sb.String()
+}
+
+func sendMessage(socket mangos.Socket, msg string) {
+	if err := socket.Send([]byte(msg)); err != nil {
+		die("API: There was an error sending the information: %s", err.Error())
+	}
+}
+
+func receiveMessage(socket mangos.Socket) string {
+	var byteArr []byte
+	var err error
+	if byteArr, err = socket.Recv(); err != nil {
+		die("API: There was an error receiving the information: %s", err.Error())
+	}
+	return string(byteArr)
+}
+
+func die(format string, v ...interface{}) {
+	fmt.Fprintln(os.Stderr, fmt.Sprintf(format, v...))
+	os.Exit(1)
+}
+
+func Start() {
+	var err error
+	if socket, err = pair.NewSocket(); err != nil {
+		die("API: can't get new pub socket: %s", err)
+	}
+	if err = socket.Dial(controllerAddress); err != nil {
+		die("API: can't listen on pub socket: %s", err.Error())
+	}
+	router := mux.NewRouter()
+	users["username"] = "password"
+
+	router.HandleFunc("/login", login)                         //POST
+	router.HandleFunc("/logout", logout)                       //DELETE
+	router.HandleFunc("/status", status)                       //GET
+	router.HandleFunc("/workloads", workloads)                 //POST
+	router.HandleFunc("/workloads/{workload_id}", getWorkload) //GET
+	router.HandleFunc("/images", images)                       //POST
+	router.HandleFunc("/images/{image_id}", getImage)          //GET
+	http.ListenAndServe(":8080", router)
 }
--- controller/controller.go
+++ controller/controller.go
@@ -1,77 +1,139 @@
 package controller
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
-	"time"
+	"strconv"
+	"strings"
 
 	"go.nanomsg.org/mangos"
-	"go.nanomsg.org/mangos/protocol/pub"
 
 	// register transports
+	"go.nanomsg.org/mangos/protocol/pair"
 	_ "go.nanomsg.org/mangos/transport/all"
 )
 
 type Workload struct {
-	ID             int
-	Filter         string
-	Name           string
-	Status         string
-	RunningJobs    int
-	FilteredImages []int
+	ID             int    `json:"ID"`
+	Filter         string `json:"Filter"`
+	Name           string `json:"Name"`
+	Status         string `json:"Status"`
+	RunningJobs    int    `json:"RunningJobs"`
+	FilteredImages []int  `json:"FilteredImages"`
 }
 
 type Image struct {
-	ID         int
-	WorkloadID int
-	Type       string
-	Name       string
+	ID         int    `json:"ID"`
+	WorkloadID int    `json:"WorkloadID"`
+	Type       string `json:"Type"`
+	Name       string `json:"Name"`
+}
+
+type WorkloadsResponse struct {
+	Workload []Workload `json:"Workload"`
+	Response string     `json:"Response"`
 }
 
 var workloads []Workload
 
 var controllerAddress = "tcp://localhost:40899"
 
+var socket mangos.Socket
+
 func die(format string, v ...interface{}) {
 	fmt.Fprintln(os.Stderr, fmt.Sprintf(format, v...))
 	os.Exit(1)
 }
 
-func date() string {
-	return time.Now().Format(time.ANSIC)
-}
-
-func Start() {
-	var sock mangos.Socket
-	var err error
-	if sock, err = pub.NewSocket(); err != nil {
-		die("can't get new pub socket: %s", err)
+func createWorkload(name string, filter string) bool {
+	if filter != "" && filter != "grayscale" && filter != "blur" {
+		fmt.Println("False")
+		return false
 	}
-	if err = sock.Listen(controllerAddress); err != nil {
-		die("can't listen on pub socket: %s", err.Error())
-	}
-	for {
-		// Could also use sock.RecvMsg to get header
-		d := date()
-		log.Printf("Controller: Publishing Date %s\n", d)
-		if err = sock.Send([]byte(d)); err != nil {
-			die("Failed publishing: %s", err.Error())
-		}
-		time.Sleep(time.Second * 3)
-	}
-}
-
-func createWorkload(name string, filter string) (int, bool) {
 	newWorkload := Workload{len(workloads), filter, name, "Scheduling", 0, make([]int, 0)}
 	workloads = append(workloads, newWorkload)
-	return len(workloads), true
+	return true
 }
 
 func getWorkload(id int) Workload {
+	if id >= len(workloads) {
+		return Workload{-1, "", "", "", -1, make([]int, 0)}
+	}
 	return workloads[id]
 }
 
 func getWorkloads() []Workload {
 	return workloads
+}
+
+func sendMessage(socket mangos.Socket, msg string) {
+	if err := socket.Send([]byte(msg)); err != nil {
+		die("Controller: There was an error sending the information: %s", err.Error())
+	}
+}
+
+func receiveMessage(socket mangos.Socket) string {
+	var byteArr []byte
+	var err error
+	if byteArr, err = socket.Recv(); err != nil {
+		die("Controller: There was an error receiving the information: %s", err.Error())
+	}
+	return string(byteArr)
+}
+
+func controllerResponses(msg string) {
+	response := ""
+	var err error
+	var rMsg []byte
+	split := strings.Split(msg, "_")
+	c, _ := strconv.Atoi(split[0])
+	split = split[1:]
+	switch c {
+	//Create Workload, send all workloads
+	case 1:
+		name := split[0]
+		filter := split[1]
+		created := createWorkload(name, filter)
+		if !created {
+			response = ("Controller: There was an error creating the workload")
+		}
+		respJSON := WorkloadsResponse{workloads, response}
+		rMsg, err = json.Marshal(respJSON)
+		if err != nil {
+			log.Fatal("Controller Error: " + err.Error())
+		}
+
+		sendMessage(socket, string(rMsg))
+		break
+
+		//send specific workload
+	case 2:
+		id, _ := strconv.Atoi(split[0])
+		workload := getWorkload(id)
+
+		rMsg, err = json.Marshal(workload)
+		if err != nil {
+			log.Fatal("Controller Error: " + err.Error())
+		}
+
+		sendMessage(socket, string(rMsg))
+		break
+	}
+}
+
+func Start() {
+	var msg string
+	var err error
+	if socket, err = pair.NewSocket(); err != nil {
+		die("Controller: can't get new pub socket: %s", err)
+	}
+	if err = socket.Listen(controllerAddress); err != nil {
+		die("Controller: can't listen on pub socket: %s", err.Error())
+	}
+	for {
+		msg = receiveMessage(socket)
+		controllerResponses(msg)
+	}
 }
